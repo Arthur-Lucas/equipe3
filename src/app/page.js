ff6837fd896d9a71d41acaa7d99321ea46c0962b
Arthur <arthurlucas92@orange.fr>
2024-03-26 10:49:33 +0100

    setup project

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Home() {
   return (
