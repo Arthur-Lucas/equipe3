9eae4296fb3dd8df004d47d6042c420201e2b12f
Matis DENE <matis.dene@edu.devinci.fr>
2024-03-27 10:17:42 +0100

    add modifs

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,4 @@
-"use client";
-
-import Image from "next/image";
 import styles from "./page.module.css";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import HomePage from "./Home/home";
-
 export default function Home() {
   return <HomePage/>;
 }
